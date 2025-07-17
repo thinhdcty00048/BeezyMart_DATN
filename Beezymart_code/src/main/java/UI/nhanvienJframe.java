b4486c4b8e7b72e42d50592997a2a9216ddf1f6b
phong123-gb <phongnty00123@gmail.com>
2025-07-17 08:51:14 +0700

    nn

--- Beezymart_code/src/main/java/UI/nhanvienJframe.java
+++ Beezymart_code/src/main/java/UI/nhanvienJframe.java
@@ -110,20 +110,20 @@ public class nhanvienJframe extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(BamBillJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(nhanvienJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(BamBillJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(nhanvienJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(BamBillJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(nhanvienJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(BamBillJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(nhanvienJframe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new BamBillJframe().setVisible(true);
+                new nhanvienJframe().setVisible(true);
             }
         });
     }
---
+++ Beezymart_code/src/main/java/Util/XAuth.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Util;
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class XAuth {
+  public static User user = null;  
+
+    private static class User {
+
+        public User() {
+        }
+    }
+}
