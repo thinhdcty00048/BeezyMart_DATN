acaeee5475078ec8dc774cfac78e210c02563cd2
phong123-gb <phongnty00123@gmail.com>
2025-07-31 08:08:26 +0700

    n

---
+++ Beezymart_code/src/main/java/Dao/CategoryDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Dao;
+
+import Entity.Category;
+
+/**
+ *
+ * @author ADMIN
+ */
+public interface CategoryDAO extends CrudDAO<Category, String> {
+    
+}
